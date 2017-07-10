f93ef28f38685d1acbddcd4f07664094f62e36be
shekjbs <shekjbs@gmail.com>
2017-07-10 23:24:36 +0530

    Login Ui Updated

--- app/src/main/java/com/example/home/gym/LoginActivity.java
+++ app/src/main/java/com/example/home/gym/LoginActivity.java
@@ -2,14 +2,22 @@ package com.example.home.gym;
 
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.skyfishjy.library.RippleBackground;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,9 +28,10 @@ import java.sql.Statement;
 public class LoginActivity extends AppCompatActivity {
     String user;
     String pass;
-
+    TextView v1,v2;
     String abc = "hello";
-
+    RelativeLayout rl;
+    Typeface typeface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,14 +40,51 @@ public class LoginActivity extends AppCompatActivity {
         final EditText userName, password;
         Button button;
 
-        userName = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        button = (Button) findViewById(R.id.login);
+        userName = (EditText) findViewById(R.id.name);
+        password = (EditText) findViewById(R.id.phone);
+        button = (Button) findViewById(R.id.register);
+
+        String type="fontb.otf";
+        typeface= Typeface.createFromAsset(getAssets(),type);
+        YoYo.with(Techniques.Landing).duration(1500).playOn(findViewById(R.id.logo));
+        final RippleBackground rippleBackground= (RippleBackground) findViewById(R.id.content);
+        rippleBackground.startRippleAnimation();
+        final Boolean bol=rippleBackground.isRippleAnimationRunning();
+        if(bol==true){
+            Handler handler=new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    rippleBackground.stopRippleAnimation();
+                }
+            },1500);
+        }
+
+        v1= (TextView) findViewById(R.id.textView5);
+        v2= (TextView) findViewById(R.id.usrs);
+        rl= (RelativeLayout) findViewById(R.id.rel);
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                rippleBackground.startRippleAnimation();
+                Boolean bols=rippleBackground.isRippleAnimationRunning();
+                if(bols==true){
+                    Handler handler=new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            rippleBackground.stopRippleAnimation();
+                        }
+                    },1500);
+                }
+//                EditText gname=(EditText) findViewById(R.id.name);
+//                EditText gphone=(EditText) findViewById(R.id.phone);
+//                EditText gaddress=(EditText) findViewById(R.id.address);
+
+
                 //Toast.makeText(getApplicationContext(),"Button clicked!",Toast.LENGTH_SHORT).show();
 
                 user = userName.getText().toString();
@@ -99,11 +145,34 @@ public class LoginActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             if (abc.equals("found")) {
-                Intent intent = new Intent(LoginActivity.this, FragmentManagement.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
 
-                Toast.makeText(getApplicationContext(), abc, Toast.LENGTH_SHORT).show();
+                YoYo.with(Techniques.SlideOutUp).duration(1000).playOn(findViewById(R.id.rel));
+                Handler handler=new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        rl.setVisibility(View.INVISIBLE);
+                        v1.setTypeface(typeface);
+                        v2.setTypeface(typeface);
+                        v1.setVisibility(View.VISIBLE);
+                        v2.setVisibility(View.VISIBLE);
+                        v2.setText(user);
+                        YoYo.with(Techniques.BounceIn).duration(1500).playOn(findViewById(R.id.textView5));
+                        YoYo.with(Techniques.BounceIn).duration(1500).playOn(findViewById(R.id.usrs));
+                    }
+                },900);
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(LoginActivity.this, FragmentManagement.class);
+                        intent.putExtra("user", user);
+                        startActivity(intent);
+
+                        Toast.makeText(getApplicationContext(), abc, Toast.LENGTH_SHORT).show();
+                    }
+                },4000);
+
             } else
                 Toast.makeText(getApplicationContext(), abc + "notfound", Toast.LENGTH_SHORT).show();
 
